efc9d9e9b9282916637e2ae865923faec899cfef
Saqib Nawab <106011021+Saqib-nawab@users.noreply.github.com>
2024-06-04 18:37:29 +0500

    added button components and welcome page

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,4 +1,4 @@
-import { StatusBar } from "expo-status-bar";
+// import { StatusBar } from "expo-status-bar";
 import { Redirect, Tabs } from "expo-router";
 import { Image, Text, View } from "react-native";
 
