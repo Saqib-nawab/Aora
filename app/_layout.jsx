e589f9839bcdbab56aeedd167775e11bbaca3e58
Saqib Nawab <106011021+Saqib-nawab@users.noreply.github.com>
2024-06-05 12:02:52 +0500

    added a sign-in page, changed auth layout header, FormFiled etc

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -39,7 +39,8 @@ const RootlLayout = () => {
 
     return (
         <Stack>
-            <Stack.Screen name='index' options={{ headerShown: false }} />
+            <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+            <Stack.Screen name="index" options={{ headerShown: false }} />
         </Stack>
     )
 }
